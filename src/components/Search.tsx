3fcc48c2954d1fb65fe6df44ef3a7ee504ab3f6c
khkh0109 <ist200120@gmail.com>
2023-07-18 22:41:13 +0900

    feat: Search 컴포넌트 추가

---
+++ src/components/Search.tsx
@@ -0,0 +1,43 @@
+import { useState, useRef } from "react";
+import SearchBar from "./SearchBar";
+import Dropdown from "./Dropdown";
+import { SearchAPI } from "../api/search";
+import { SearchSuggestions } from "../types";
+
+function Search() {
+  const [isInputValue, setIsInputValue] = useState<boolean>(false);
+  const [dropdownData, setDropdownData] = useState<SearchSuggestions>([]);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const changeIsInputValue = (value: string | undefined) => {
+    if (value) {
+      setIsInputValue(true);
+    } else {
+      setIsInputValue(false);
+    }
+  };
+
+  const searchRequest = async (value: string) => {
+    try {
+      const data = await SearchAPI.get(value);
+      setDropdownData(data);
+      console.info("calling api");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <>
+      <SearchBar
+        inputRef={inputRef}
+        isInputValue={isInputValue}
+        changeIsInputValue={changeIsInputValue}
+        searchRequest={searchRequest}
+      />
+      <Dropdown isInputValue={isInputValue} dropdownData={dropdownData} />
+    </>
+  );
+}
+
+export default Search;
