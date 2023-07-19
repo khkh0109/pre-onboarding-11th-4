8dd371452a139ddd6bea6634377e47f463744782
khkh0109 <ist200120@gmail.com>
2023-07-19 23:04:15 +0900

    design: 스타일 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import Search from "./components/Search";
 
 function App() {
   return (
-    <>
+    <div className="flex h-full flex-col items-center justify-center p-20">
       <Title />
       <Search />
-    </>
+    </div>
   );
 }
 
