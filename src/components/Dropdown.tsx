5f07db363feeb8ce05f49d8071a48910a8cceda3
khkh0109 <ist200120@gmail.com>
2023-07-19 17:05:32 +0900

    feat: 추천검색어 선택 기능 추가

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -3,17 +3,29 @@ import { SearchSuggestions } from "../types";
 interface DropdownProps {
   isInputValue: boolean;
   dropdownData: SearchSuggestions;
+  inputRef: React.RefObject<HTMLInputElement>;
 }
 
-function Dropdown({ isInputValue, dropdownData }: DropdownProps) {
+function Dropdown({ isInputValue, dropdownData, inputRef }: DropdownProps) {
+  const updateInputValue = (e: React.MouseEvent<HTMLLIElement>) => {
+    if (inputRef.current === null) return;
+    inputRef.current.value = e.currentTarget.innerText;
+  };
+
   return (
     <>
       {dropdownData.length === 0 && isInputValue ? (
         <div>검색어 없음</div>
       ) : (
-        <ul className={isInputValue ? "" : "hidden"}>
+        <ul className={`${isInputValue ? "" : "hidden"} `}>
           {dropdownData.map(item => (
-            <li key={item.sickCd}>{item.sickNm}</li>
+            <li
+              key={item.sickCd}
+              className="cursor-pointer"
+              onClick={updateInputValue}
+            >
+              {item.sickNm}
+            </li>
           ))}
         </ul>
       )}
