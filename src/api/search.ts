fb98dd473b6ae51eb7d24a8b937df595dc51b5dc
khkh0109 <ist200120@gmail.com>
2023-07-19 16:36:25 +0900

    feat: 캐싱 기능 추가

--- src/api/search.ts
+++ src/api/search.ts
@@ -1,19 +1,28 @@
-import axios from "axios";
-import { SearchSuggestions } from "../types";
-
-const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000",
-});
-
 export class SearchAPI {
+  static #BASE_URL = "http://localhost:4000";
   static #ENDPOINT = "sick";
 
-  static async get(value: string): Promise<SearchSuggestions> {
-    const response = await axiosInstance.get(`/${this.#ENDPOINT}`, {
-      params: {
-        q: value,
-      },
-    });
-    return response.data;
+  static async get(value: string) {
+    const URI = `${this.#BASE_URL}/${this.#ENDPOINT}?q=${value}`;
+
+    const cache = await caches.open("test");
+    const responseCache = await cache.match(URI);
+
+    if (responseCache) {
+      const cacheData = await responseCache.clone().json();
+      return cacheData;
+    }
+
+    if (responseCache === undefined) {
+      const response = await fetch(URI);
+      const data = await response.clone().json();
+
+      if (response?.ok) {
+        cache.put(URI, response);
+      }
+
+      console.info("calling api");
+      return data;
+    }
   }
 }
